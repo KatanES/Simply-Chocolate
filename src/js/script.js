f823cc0ea986ef54dfd14a954cfab5d877ba2ce4
KatanES <businesskatan.es@gmail.com>
2023-04-11 19:28:22 +0200

    Revert "Merge pull request #81 from KatanES/ed-reviews-tu-one"
    This reverts commit 37068da3e891b0c7e7b09d12ba006bd36b1b62b3, reversing
    changes made to 4026615980c0a655a013b9f833401f8a92520d7a.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,27 @@
-import Swiper from 'swiper/bundle';
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  loop: true,
+  speed: 1000,
+
+
+
+});
+
 
 const gallary = new Swiper('.gallary', {
-  navigation: {
-    nextEl: '.course-next',
-    prevEl: '.course-prev',
-  },
+  // navigation: {
+  //   nextEl: '.course-next',
+  //   prevEl: '.course-prev',
+  // },
   pagination: {
     el: '.gallary-pagination',
     type: 'bullets',
@@ -13,20 +30,8 @@ const gallary = new Swiper('.gallary', {
   },
   loop: true,
   speed: 1000,
-    //   breakpoints: { 
-    //     320: {           
-    //        slidesPerView: 1, 
-    //        spaceBetween: 20,
-    //   },      
-    //     768: {          
-    //        slidesPerView: 2, 
-    //        spaceBetween: 20,
-    //   },        
-    //     1199: {           
-    //        slidesPerView: 3, 
-    //        spaceBetween: 20,
-    //     }, 
-    // },
+
+
 });
 
 
