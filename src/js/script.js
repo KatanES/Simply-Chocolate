f7610e8dfaf0795826b6b219bb72971f7ee625cf
EduardMLT <eddku@i.ua>
2023-04-11 14:34:18 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,20 @@
-const swiper = new Swiper('.swiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-    dynamicBullets: true,
-  },
-  loop: true,
-  speed: 1000,
+// const swiper = new Swiper('.swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+//   loop: true,
+//   speed: 1000,
 
 
 
-});
+// });
 
 
 const gallary = new Swiper('.gallary', {
